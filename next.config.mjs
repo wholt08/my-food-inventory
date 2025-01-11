21703d9b596531b2e67a2884158835a1c81f5e37
Billy <wholt08@gmail.com>
2025-01-11 12:27:54 -0800

    Add GitHub Actions workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',
+    basePath: '/food-inventory',
+    images: {
+      unoptimized: true,
+    }
+  }
+  
+  module.exports = nextConfig
